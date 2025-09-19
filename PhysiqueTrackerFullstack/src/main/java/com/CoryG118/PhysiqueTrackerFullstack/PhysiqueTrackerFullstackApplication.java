9e7f98303a380df1536b8edd843914f3a671438f
Cory Graham <ghmcry48@gmail.com>
2025-09-18 22:26:00 -0700

    Initalized project

---
+++ PhysiqueTrackerFullstack/src/main/java/com/CoryG118/PhysiqueTrackerFullstack/PhysiqueTrackerFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.CoryG118.PhysiqueTrackerFullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysiqueTrackerFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysiqueTrackerFullstackApplication.class, args);
+	}
+
+}
---
+++ PhysiqueTrackerFullstack/src/test/java/com/CoryG118/PhysiqueTrackerFullstack/PhysiqueTrackerFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CoryG118.PhysiqueTrackerFullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysiqueTrackerFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
